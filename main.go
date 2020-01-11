8391a0eb20f1b0cda082f34e9cbdf30cf8df9e89
timschreiner <tim@outdoorsy.com>
2020-01-11 08:38:33 +0700

    changed some imports

--- main.go
+++ main.go
@@ -10,8 +10,8 @@ import (
 	"os"
 	"strings"
 
-	"github.com/cheekybits/genny/out"
-	"github.com/cheekybits/genny/parse"
+	"github.com/timschreiner/genny/out"
+	"github.com/timschreiner/genny/parse"
 )
 
 /*
